75a5007967eea08d740d2d434498f6a73516d837
suraj-sharma3 <suraj.sharma13395@gmail.com>
2025-01-31 18:23:36 +0530

    Added lombok dependency in everJournal project and used related annotations

--- everJournal/src/main/java/com/esteam/everJournal/entity/JournalEntry.java
+++ everJournal/src/main/java/com/esteam/everJournal/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.esteam.everJournal.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
@@ -15,36 +19,4 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
